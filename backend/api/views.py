fdfdd0d01c839b02af15be240a133a7cbb6f5bbf
Code bee <codebee345@outlook.com>
2023-09-08 17:21:51 +0100

    stored user in a varible in get user function

--- backend/api/views.py
+++ backend/api/views.py
@@ -86,8 +86,9 @@ def login(request, *args, **kwargs):
 @api_view(['GET'])
 def logout(request):
     if request.user is not None:
+        username = request.user.username
         auth.logout(request)
-        return Response({'message': 'loogged out user succesfully', 'user': request.user.username})
+        return Response({'message': 'loogged out user succesfully', 'user': username})
     else:
         return Response({'message': 'user is none'})
 
